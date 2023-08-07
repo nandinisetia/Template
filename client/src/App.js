f4ba146e2f18e02447c8b6ceb533c1d4d93bbcce
Garvit Batra <batra.garvit28@gmail.com>
2023-08-07 20:09:52 +0530

    registration form done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import React from "react";
+import Registration from "./components/Registration";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Registration />
     </div>
   );
 }
