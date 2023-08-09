c28fb06d07aa64edf50460716707c8c841e4d94f
Nandini Setia <72308664+nandinisetia@users.noreply.github.com>
2023-08-09 19:09:58 +0530

    changes done

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Nav from './Nav'
 import Home from './Home'
-
+import Cards from './Cards'
+import Grid from './Grid'
 export default function Landing() {
   return (
     <div>
       <Nav/>
       <Home/>
+      <Cards/>
+      <Grid/>
     </div>
   )
 }
