8f28af1c5c60eefad82df03f9597b5c43a768545
Noor <noordeep29oct@gmail.com>
2023-08-09 23:40:35 +0530

    router

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -52,7 +52,7 @@ const navigate = useNavigate();
               </button>
               <button onClick={signUpPage} className="bg-[#424642] text-white hover:bg-[#C06014] px-4 py-2 rounded-full">
                 Sign Up
-              </Link>
+              </button>
             </div>
           </div>
         </div>
