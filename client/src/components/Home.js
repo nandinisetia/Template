0dec801191caa4b2583a51ebb260aa1eb706d032
Noor <noordeep29oct@gmail.com>
2023-08-09 18:19:48 +0530

    updated home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import pic from '../assets/home.jpg'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <div class="flex flex-row justify-center items-center text-center w-full  pb-12">
+      <div class="w-full sm:w-1/2 pb-8 text-center">
+        <h1 class="font-semibold text-l sm:text-2xl">Financial Literacy</h1>
+         <p>Lorem Ipsum</p>
+      </div>
+    <img
+      className="w-1/2 h-200"
+      alt="mypic"
+      src={pic}
+    />
+</div>
+
   )
 }
