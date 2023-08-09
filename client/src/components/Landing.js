15abfb2c20b6097f49c3900774eece72c6868aeb
Nandini Setia <72308664+nandinisetia@users.noreply.github.com>
2023-08-09 16:42:58 +0530

    updates2

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react'
+import Nav from './Nav'
+import Home from './Home'
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <Nav/>
+      <Home/>
+    </div>
+  )
 }
